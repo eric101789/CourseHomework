d00ed60dfcdfbc9288bd526785fa9d9863f24fd5
eric10179 <eric10179@gmail.com>
2022-03-28 23:24:52 +0800

    第五章作業_4_final

---
+++ main/PrintStars.py
@@ -0,0 +1,19 @@
+number = int(input("Please input a number (4~9): "))
+
+# 第一種星星
+for i in range(1, number+1):
+    print('*' * i)
+
+# 第二種星星
+for i in range(0, number+1):
+    print(' ' * (number-i) + '*' * i)
+
+# 第三種星星
+for i in range(0, number+1):
+    print(' ' * (number-i) + '*' * (2*i-1))
+
+# 第四種星星
+for i in range(0, number+1):
+    print(' ' * (number-i) + '*' * (2*i-1))
+for i in range(number, 0, -1):
+    print(' ' * (number-i) + '*' * (2*i-1))

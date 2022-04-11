d6669249f1028bbaa64635b7c6b850a9cc4f8ce3
eric101789 <eric10179@gmail.com>
2022-04-11 20:06:46 +0800

    04/11 修改PrintStars 第四種星星 下半部分

--- main/PrintStars.py
+++ main/PrintStars.py
@@ -1,7 +1,7 @@
 number = int(input("Please input a number (4~9): "))
 
 # 第一種星星
-for i in range(1, number+1):
+for i in range(0, number+1):
     print('*' * i)
 
 # 第二種星星
@@ -15,5 +15,5 @@ for i in range(0, number+1):
 # 第四種星星
 for i in range(0, number+1):
     print(' ' * (number-i) + '*' * (2*i-1))
-for i in range(number, 0, -1):
+for i in range(number-1, 0, -1):
     print(' ' * (number-i) + '*' * (2*i-1))
